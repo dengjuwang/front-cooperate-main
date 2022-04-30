74b61f0792706d657a18f982d6b65743566b498d
wanghaonan12 <1470918223@qq.com>
2022-04-30 22:17:05 +0800

    feat: answer页面添加

--- src/views/Answer.vue
+++ src/views/Answer.vue
@@ -1,9 +1,53 @@
 <template>
-  <h2>等你来答</h2>
+  <div class="card">
+    <div v-for="(item, index) in btnList" :key="index">
+      <div class="btn iconfont">
+        {{ item.icon }}
+        {{ item.info }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      btnList: [
+        {
+          icon: '&#xe60f;',
+          info: '为你推荐'
+        },
+        {
+          icon: '&#xe63e;',
+          info: '邀请回答'
+        },
+        {
+          icon: '&#xe601;',
+          info: '最新问题'
+        },
+        {
+          icon: '&#xe708;',
+          info: '人气问题'
+        }
+      ]
+    }
+  }
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import '../../style/iconfont.css';
+.card {
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  .btn {
+    background: rgba(51, 51, 51, 0.14);
+    padding: 10px;
+    margin: 10px;
+    border-radius: 10px;
+    
+  }
+}
+</style>
