e0e96df1e31fbed9e33441a35d66353f6cbaa14b
wanghaonan12 <1470918223@qq.com>
2022-04-30 23:35:00 +0800

    refactor: answer页面的完善

--- src/views/Answer.vue
+++ src/views/Answer.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="card">
-    <div v-for="(item, index) in btnList" :key="index">
-      <div class="btn iconfont">
-        {{ item.icon }}
-        {{ item.info }}
+  <v-main>
+    <div class="flex">
+      <div v-for="(item, index) in btnList" :key="index">
+        <v-btn color="primary" class="m-2">
+          <v-icon>
+            {{ item.icon }}
+          </v-icon>
+          {{ item.info }}
+        </v-btn>
       </div>
     </div>
-  </div>
+    <v-card class="flex mt-3" v-for="(item, index) in newList" :key="index">
+      <v-img
+        :src="item.imgUrl"
+        max-width="170"
+        max-height="170"
+        min-width="170"
+        min-height="170"
+        class="grey darken-4"
+      ></v-img>
+      <div>
+        <v-card-title class="text-h6"> {{ item.title }} </v-card-title>
+        <v-card-text> {{ item.text }} </v-card-text>
+      </div>
+    </v-card>
+  </v-main>
 </template>
 
 <script>
@@ -15,20 +33,52 @@ export default {
     return {
       btnList: [
         {
-          icon: '&#xe60f;',
+          icon: 'mdi-star',
           info: '为你推荐'
         },
         {
-          icon: '&#xe63e;',
+          icon: 'mdi-account-multiple-plus',
           info: '邀请回答'
         },
         {
-          icon: '&#xe601;',
+          icon: 'mdi-new-box',
           info: '最新问题'
         },
         {
-          icon: '&#xe708;',
+          icon: 'mdi-fire',
           info: '人气问题'
+        }
+      ],
+      newList: [
+        {
+          imgUrl: 'https://img0.baidu.com/it/u=158562056,22340437&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=400',
+          title: '海绵宝宝',
+          text: '知识不能替代友谊，比起失去你，我宁愿做个白痴 。'
+        },
+        {
+          imgUrl: 'https://img1.baidu.com/it/u=1657365165,1891888622&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=750',
+          title: '海绵宝宝',
+          text: '有青苔的地方就有文明'
+        },
+        {
+          imgUrl: 'https://img1.baidu.com/it/u=3921974019,2915255256&fm=253&fmt=auto&app=138&f=JPEG?w=533&h=300',
+          title: '海绵宝宝',
+          text: '如果你总是让人踩过你的头，你永远都不会得到你想要的东西。'
+        },
+        {
+          imgUrl: 'https://img0.baidu.com/it/u=2020775051,1757117215&fm=253&fmt=auto&app=138&f=JPEG?w=667&h=500',
+          title: '海绵宝宝',
+          text: '这里埋葬着章鱼哥的希望和梦想。'
+        },
+        {
+          imgUrl: 'https://img0.baidu.com/it/u=2676171483,840293986&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=400',
+          title: '海绵宝宝',
+          text: '海绵宝宝，我只想告诉你一件事情，派大星也是需要人疼的！'
+        },
+        {
+          imgUrl: 'https://img2.baidu.com/it/u=1625129896,2962725842&fm=253&fmt=auto&app=138&f=JPEG?w=666&h=500',
+          title: '海绵宝宝',
+          text: '我要两个冰淇淋，一个给我吃一个给我的好朋友海绵宝宝。'
         }
       ]
     }
@@ -38,16 +88,7 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../style/iconfont.css';
-.card {
-  padding: 10px;
+.flex {
   display: flex;
-  flex-wrap: wrap;
-  .btn {
-    background: rgba(51, 51, 51, 0.14);
-    padding: 10px;
-    margin: 10px;
-    border-radius: 10px;
-    
-  }
 }
 </style>
